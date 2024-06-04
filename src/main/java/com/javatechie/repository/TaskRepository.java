1032a72de56c712f3c19c9bc59f84a10371872a9
Nguyen Trung Nghia <ntn1804@gmail.com>
2024-06-04 15:58:10 +0700

    complete project

--- src/main/java/com/javatechie/repository/TaskRepository.java
+++ src/main/java/com/javatechie/repository/TaskRepository.java
@@ -1,8 +1,6 @@
 package com.javatechie.repository;
 
 import com.javatechie.model.Task;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Pageable;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.data.mongodb.repository.Query;
 
